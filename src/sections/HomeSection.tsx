59e197bd564d6537843ab05dac295f6c9bf0c14c
aimme14 <aylopezm@ufpso.edu.co>
2025-06-16 15:38:02 -0500

    areglo

--- src/sections/HomeSection.tsx
+++ src/sections/HomeSection.tsx
@@ -1,12 +1,7 @@
-import { FileText, Calendar, FileSpreadsheet, ShoppingBag } from 'lucide-react'
 import { ThemeContextProps } from '@/interfaces/context.interface'
 import CarouselInfo from '#/pages/home/CarouselInfo'
-import { motion } from 'framer-motion'
-import { useState } from 'react'
-import { cn } from '@/lib/utils'
 
 const HomeSection = ({ theme }: ThemeContextProps) => {
-  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
   return (
     <>
       <CarouselInfo
@@ -22,20 +17,6 @@ const HomeSection = ({ theme }: ThemeContextProps) => {
 
 export default HomeSection
 /*---------------------------------------------------------------------------------------------------------*/
-
-/*--------------------------------------------------tools--------------------------------------------------*/
-interface Feature {
-  title: string,
-  description: string,
-  icon: React.ForwardRefExoticComponent<any>
-}
-
-const features: Feature[] = [
-  { icon: ShoppingBag, title: 'Almacenamiento', description: 'Almacena formatos de manera segura' },
-  { icon: FileSpreadsheet, title: 'Presentaciones', description: 'Accede a hojas de vida y entregables para auditorias' },
-  { icon: Calendar, title: 'Administración', description: 'Gestiona equipos biomédicos con nuestro sistema de calendarios' },
-  { icon: FileText, title: 'Documentación', description: 'Genera informes y accede a ellos de manera flexible' },
-]
 
 export const heroItems = [
   {
